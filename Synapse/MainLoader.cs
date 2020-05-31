5905cf61a601d24e0408cfadfad2bfacb2bd2ead
GrafDimenzio <crafterwolfhd@gmail.com>
2020-05-31 15:39:52 +0200

    Create Directory

--- Synapse/MainLoader.cs
+++ Synapse/MainLoader.cs
@@ -1,6 +1,30 @@
-﻿namespace Synapse
+﻿using MEC;
+using System.IO;
+
+namespace Synapse
 {
     public class MainLoader
     {
+        /// <summary>This method is called by the Scp Server in the assembly-csharp</summary>
+        public static void LoaderExecutionCode()
+        {
+            if (!Directory.Exists(PluginManager.ConfigDirectory))
+                Directory.CreateDirectory(PluginManager.ConfigDirectory);
+
+            if (!File.Exists(PluginManager.ConfigPath))
+                File.Create(PluginManager.ConfigPath).Close();
+
+            if (!Directory.Exists(PluginManager.MainPluginDirectoty))
+                Directory.CreateDirectory(PluginManager.MainPluginDirectoty);
+
+            if (!Directory.Exists(PluginManager.ServerPluginDirectory))
+                Directory.CreateDirectory(PluginManager.ServerPluginDirectory);
+
+            if (!Directory.Exists(PluginManager.DependenciesDirectory))
+                Directory.CreateDirectory(PluginManager.DependenciesDirectory);
+
+            CustomNetworkManager.Modded = true;
+            Timing.RunCoroutine(PluginManager.LoadPlugins());
+        }
     }
 }
---
+++ Synapse/PluginManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using MEC;
+using System.IO;
+using System.Reflection;
+
+namespace Synapse
+{
+    public class PluginManager
+    {
+        public static List<Assembly> LoadedDependencies { get; } = new List<Assembly>();
+        public static string SynapseDirectoty { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),"Synapse");
+        public static string MainPluginDirectoty { get; } = Path.Combine(SynapseDirectoty, "Plugins");
+        public static string ServerPluginDirectory { get; } = Path.Combine(MainPluginDirectoty,$"Server{ServerStatic.ServerPort} Plugins");
+        public static string DependenciesDirectory { get; } = Path.Combine(SynapseDirectoty,"dependencies");
+        public static string ConfigDirectory { get; } = Path.Combine(SynapseDirectoty, "ServerConfigs");
+        public static string ConfigPath { get; } = ConfigDirectory + $"Server{ServerStatic.ServerPort}-config.yml";
+
+        public static IEnumerator<float> LoadPlugins()
+        {
+            yield return Timing.WaitForSeconds(0.5f);
+
+
+        }
+
+        private static void LoadDependencies()
+        {
+            string[] depends = Directory.GetFiles(DependenciesDirectory);
+
+            foreach (string dll in depends)
+            {
+                if (!dll.EndsWith(".dll")) continue;
+
+                if (IsLoaded(dll))
+                    return;
+            }
+        }
+
+        private static bool IsLoaded(string path)
+        {
+            foreach (Assembly assembly in LoadedDependencies)
+            {
+                if (assembly.Location == path)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
