073cf461ec013044249575ed64b2624160697fc4
Robin <moelrobi@gmail.com>
2020-06-01 00:07:37 +0200

    Add Color to the Information Log

--- Synapse/Log.cs
+++ Synapse/Log.cs
@@ -14,7 +14,7 @@ namespace Synapse
         public static void Info(string message)
         {
             var assembly = Assembly.GetCallingAssembly();
-            ServerConsole.AddLog($"[INFO] [{assembly.GetName().ToString().Split(',')[0]}] {message}");
+            ServerConsole.AddLog($"[INFO] [{assembly.GetName().ToString().Split(',')[0]}] {message}", ConsoleColor.Blue);
         }
 
         /// <summary>Sends a Warn in the Console</summary>
@@ -22,7 +22,7 @@ namespace Synapse
         public static void Warn(string message)
         {
             var assembly = Assembly.GetCallingAssembly();
-            ServerConsole.AddLog($"[WARN] [{assembly.GetName().ToString().Split(',')[0]}] {message}",ConsoleColor.Yellow);
+            ServerConsole.AddLog($"[WARN] [{assembly.GetName().ToString().Split(',')[0]}] {message}", ConsoleColor.Yellow);
         }
 
         /// <summary>Sends a Error in the Console</summary>
@@ -30,7 +30,7 @@ namespace Synapse
         public static void Error(string message)
         {
             var assembly = Assembly.GetCallingAssembly();
-            ServerConsole.AddLog($"[ERROR] [{assembly.GetName().ToString().Split(',')[0]}] {message}",ConsoleColor.Red);
+            ServerConsole.AddLog($"[ERROR] [{assembly.GetName().ToString().Split(',')[0]}] {message}", ConsoleColor.Red);
         }
     }
 }
