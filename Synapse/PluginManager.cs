af0c44b90e7856505ce0d2ecc374100b2d462e89
GrafDimenzio <crafterwolfhd@gmail.com>
2020-05-31 15:52:44 +0200

    Bug Fix

--- Synapse/PluginManager.cs
+++ Synapse/PluginManager.cs
@@ -8,19 +8,21 @@ namespace Synapse
 {
     public class PluginManager
     {
-        public static List<Assembly> LoadedDependencies { get; } = new List<Assembly>();
+        //Variablen
+        private static List<Assembly> LoadedDependencies = new List<Assembly>();
         public static string SynapseDirectoty { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),"Synapse");
         public static string MainPluginDirectoty { get; } = Path.Combine(SynapseDirectoty, "Plugins");
-        public static string ServerPluginDirectory { get; } = Path.Combine(MainPluginDirectoty,$"Server{ServerStatic.ServerPort} Plugins");
+        public static string ServerPluginDirectory { get; } = Path.Combine(MainPluginDirectoty,$"Server{ServerConsole.Port} Plugins");
         public static string DependenciesDirectory { get; } = Path.Combine(SynapseDirectoty,"dependencies");
         public static string ConfigDirectory { get; } = Path.Combine(SynapseDirectoty, "ServerConfigs");
-        public static string ConfigPath { get; } = ConfigDirectory + $"Server{ServerStatic.ServerPort}-config.yml";
+        public static string ConfigPath { get; } = Path.Combine(ConfigDirectory + $"Server{ServerConsole.Port}-config.yml");
 
+        //Methoden
         public static IEnumerator<float> LoadPlugins()
         {
             yield return Timing.WaitForSeconds(0.5f);
 
-
+            LoadDependencies();
         }
 
         private static void LoadDependencies()
