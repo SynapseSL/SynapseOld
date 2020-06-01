bb7b59033e98c2038ab1ab791d2712311ba8719d
Robin <moelrobi@gmail.com>
2020-06-01 16:53:40 +0200

    ServerNamePatch Event modification

--- Synapse/Events/Patches/ServerNamePatch.cs
+++ Synapse/Events/Patches/ServerNamePatch.cs
@@ -8,7 +8,7 @@ namespace Synapse.Events.Patches
         public static void Postfix(ServerConsole __instance)
         {
             ServerConsole._serverName = ServerConsole._serverName.Replace("<size=1>SM119.0.0</size>", "");
-            ServerConsole._serverName += $" <color=#00000000><size=1>SM119.{1}.{0}.{0} Synaps</size></color>";
+            ServerConsole._serverName += $" <color=#66ff3300><size=1>SMSynapse-ModLoader v.0.1-Alpha</size></color>";
             Log.Info(ServerConsole._serverName);
         }
     }
