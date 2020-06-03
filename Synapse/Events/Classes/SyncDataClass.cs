0a74907c4e5ff9a89200dd284d58f4552d21d6b1
Robin Möller <moelrobi@gmail.com>
2020-06-03 12:16:26 +0200

    SyncDataEvent

--- Synapse/Events/Classes/SyncDataClass.cs
+++ Synapse/Events/Classes/SyncDataClass.cs
@@ -1,7 +1,15 @@
-﻿namespace Synapse.Events.Classes
+﻿using UnityEngine;
+
+namespace Synapse.Events.Classes
 {
     public class SyncDataClass
     {
+        public ReferenceHub Player { get; internal set; }
         
+        public int State { get; internal set; }
+        
+        public Vector2 Speed { get; set; }
+
+        public bool Allow { get; set; } = true;
     }
 }
