703cfc6fa56f697ce79b72d4c855de7e0fa52a6c
Robin Möller <moelrobi@gmail.com>
2020-06-23 11:47:55 +0200

    Update for independent DoorInteraction

--- Synapse/Events/Patches/DoorInteractPatch.cs
+++ Synapse/Events/Patches/DoorInteractPatch.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using Harmony;
 using Synapse.Api;
 using UnityEngine;
@@ -10,20 +12,60 @@ namespace Synapse.Events.Patches
     {
         public static bool Prefix(PlayerInteract __instance, GameObject doorId)
         {
+            var allowTheAccess = true;
+            Door door = null;
             try
             {
-                var allowed = true;
-                var door = doorId.GetComponent<Door>();
-                var player = __instance.GetPlayer();
+                if (!__instance._playerInteractRateLimit.CanExecute() ||
+                    (__instance._hc.CufferId > 0 && !PlayerInteract.CanDisarmedInteract) || doorId == null ||
+                    __instance._ccm.CurClass == RoleType.None || __instance._ccm.CurClass == RoleType.Spectator ||
+                    !doorId.TryGetComponent(out door) || !((door.buttons.Count == 0)
+                        ? __instance.ChckDis(doorId.transform.position)
+                        : door.buttons.Any(item => __instance.ChckDis(item.transform.position)))) return false;
+                
+                __instance.OnInteract();
+                
+                if (__instance._sr.BypassMode) allowTheAccess = true;
+                else if (door.PermissionLevels.HasPermission(Door.AccessRequirements.Checkpoints) &&
+                         __instance._ccm.CurRole.team == Team.SCP) allowTheAccess = true;
+                else
+                {
+                    try
+                    {
+                        if (door.PermissionLevels == 0)
+                        {
+                            allowTheAccess = !door.locked;
+                        } 
+                        else if (!door.RequireAllPermissions)
+                        {
+                            var itemPerms = __instance._inv.GetItemByID(__instance._inv.curItem).permissions;
+                            allowTheAccess = itemPerms.Any(p =>
+                                door.backwardsCompatPermissions.TryGetValue(p, out var flag) &&
+                                door.PermissionLevels.HasPermission(flag));
+                        }
+                        else allowTheAccess = false;
+                    }
+                    catch
+                    {
+                        allowTheAccess = false;
+                    }
+                }
+                
+                Events.InvokeDoorInteraction(__instance.gameObject.GetPlayer(), door, ref allowTheAccess);
+                
+                if(allowTheAccess) door.ChangeState(__instance._sr.BypassMode);
+                else __instance.RpcDenied(doorId);
 
-                Events.InvokeDoorInteraction(player, door, ref allowed);
-
-                return allowed;
+                return false;
             }
             catch (Exception e)
             {
                 Log.Error($"DoorInteraction Error: {e}");
-                return true;
+                
+                if(allowTheAccess && door != null)
+                    door.ChangeState(__instance._sr.BypassMode);
+                else __instance.RpcDenied(doorId);
+                return false;
             }
         }
     }
