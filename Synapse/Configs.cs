488d854ec4ca7468dcc818275f19f16e96e760bc
GrafDimenzio <crafterwolfhd@gmail.com>
2020-06-23 13:02:17 +0200

    small fixes

--- Synapse/Configs.cs
+++ Synapse/Configs.cs
@@ -16,8 +16,8 @@ namespace Synapse
         {
             if (_config == null) _config = new YamlConfig(Path.Combine(PluginManager.ServerConfigDirectory, "server-config.yml"));
 
-            RequiredForFemur = _config.GetInt("synapse.femur",1);
-            RemoteKeyCard = _config.GetBool("synapse.remote.keyCard", true);
+            RequiredForFemur = _config.GetInt("synapse_femur",1);
+            RemoteKeyCard = _config.GetBool("synapse_remote_keycard", true);
         }
     }
 }
