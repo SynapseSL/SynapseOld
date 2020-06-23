af3c348386199724284a55ac56cb4f70c9823c8a
Robin Möller <moelrobi@gmail.com>
2020-06-23 20:37:31 +0200

    Set the default config for remote keycard to false.

--- Synapse/Configs.cs
+++ Synapse/Configs.cs
@@ -17,7 +17,7 @@ namespace Synapse
             if (_config == null) _config = new YamlConfig(Path.Combine(PluginManager.ServerConfigDirectory, "server-config.yml"));
 
             RequiredForFemur = _config.GetInt("synapse_femur",1);
-            RemoteKeyCard = _config.GetBool("synapse_remote_keycard", true);
+            RemoteKeyCard = _config.GetBool("synapse_remote_keycard", false);
         }
     }
 }
