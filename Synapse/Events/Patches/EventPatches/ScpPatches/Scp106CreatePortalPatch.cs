e88ba8e108db6803b597f128912a12efb7d5c26d
Robin Möller <moelrobi@gmail.com>
2020-08-11 09:24:51 +0200

    + Scp106CreatePortalEvent

---
+++ Synapse/Events/Patches/EventPatches/ScpPatches/Scp106CreatePortalPatch.cs
@@ -0,0 +1,16 @@
+﻿using Harmony;
+using Synapse.Api;
+
+namespace Synapse.Events.Patches
+{
+    [HarmonyPatch(typeof(Scp106PlayerScript), nameof(Scp106PlayerScript.CallCmdMakePortal))]
+    public class Scp106CreatePortalPatch
+    {
+        private static bool Prefix(Scp106PlayerScript __instance)
+        {
+            var allow = true;
+            Events.InvokeScp106CreatePortalEvent(__instance.gameObject.GetPlayer(), ref allow);
+            return allow;
+        }
+    }
+}
